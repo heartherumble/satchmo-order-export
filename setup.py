5e25e5421754b7b0edf64b9a4258fc7d036c1193
Trent Jurewicz <tjurewicz@gmail.com>
2009-11-10 16:24:52 -0600

    added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='order_export',
+    version='0.01',
+    description='Reusable Django app to export Satchmo order data.',
+    author='Trent Jurewicz',
+    author_email='tjurewicz@gmail.com',
+    packages=find_packages(),
+)
