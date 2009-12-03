46ffecf3a9e622a90bd8612cf5a8e858998eb171
Trent Jurewicz <trent@lime.(none)>
2009-12-02 20:49:08 -0600

    updated urls.py to remove hardcoded path to order_export template

--- order_export/urls.py
+++ order_export/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('order_export.views',
-    (r'^new_orders.csv$', 'admin_order_export', {'status':'New', 'template':'admin/order_export/orders.csv'}, 'admin_new_order_export'),
+    (r'^new_orders.csv$', 'admin_order_export', {'status':'New'}, 'admin_new_order_export'),
 )
